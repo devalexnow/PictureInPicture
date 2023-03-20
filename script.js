00192794e39eea9aedaf68d2a2a6a9c185880797
devalexnow <a.nowak.dev@gmail.com>
2023-03-20 19:56:49 +0100

    picture in picture actually

---
+++ script.js
@@ -0,0 +1,27 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Promt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        //Catch error
+    }
+}
+
+button.addEventListener('click', async () => {
+// Disable button
+button.disabled = true;
+// Start Picture in Picture
+await videoElement.requestPictureInPicture();
+// Reset the button
+button.disabled = false;
+});
+
+//On load
+selectMediaStream();
